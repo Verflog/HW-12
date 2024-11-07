aa1a1806075063107ec77bcbc028ea05bd8ce70b
Verflog <pnbaranova@gmail.com>
2024-11-07 17:02:15 +0300

    HW-12

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
 public class Main {
     public static void main(String[] args) {
+        Author author1 = new Author("Fedor", "Dostoevskiy");
+        Book book1 = new Book("Igrok", author1, 1866);
+        Author author2 = new Author("Lev", "Tolstoy");
+        Book book2 = new Book("Zelenaya palochka", author2, 1905);
+        System.out.println(book1.getTitle() + " " + book1.getAuthor() + " " + book1.getPublicationYear());
+        System.out.println(book2.getTitle() + " " + book2.getAuthor() + " " + book2.getPublicationYear());
+        book2.setPublicationYear(1911);
+        System.out.println(book2.getTitle() + " " + book2.getAuthor() + " " + book2.getPublicationYear());
     }
 }
